d502dab4a731af483f215c1ce2a06bd599d492c5
Jonny Noone <jonny.f.noone@gmail.com>
2023-03-27 17:56:30 +0100

    01a complete

---
+++ 01/a.php
@@ -0,0 +1,20 @@
+<?php
+
+// Starting floor
+$floor = 0;
+
+// Read input
+$file = fopen('data.txt', 'r');
+$input = fread($file, filesize('data.txt'));
+fclose($file);
+
+// Parse instructions
+for($i = 0; $i < strlen($input); $i++) {
+    if($input[$i] === '(') {
+        $floor++;
+    } elseif($input[$i] === ')') {
+        $floor--;
+    }
+}
+
+echo $floor;
