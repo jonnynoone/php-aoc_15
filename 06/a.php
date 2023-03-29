db896d672b86f0eda03e76847168b7bfa516746b
Jonny Noone <jonny.f.noone@gmail.com>
2023-03-29 14:47:44 +0100

    06a complete

--- 06/a.php
+++ 06/a.php
@@ -1,38 +1,33 @@
 <?php
 
+// Create lights grid
 $grid = array_fill(0, 1000, array_fill(0, 1000, 0));
 
-$input = "turn on 0,0 through 999,999";
+// Get input
+$file = fopen('data.txt', 'r');
+$input = fread($file, filesize('data.txt'));
+fclose($file);
 
-$bang = explode(' ', $input);
+$instructions = explode("\n", $input);
 
-// Grab command from input
-$command = array_shift($bang);
-if ($command === "turn") {
-    $command .= " " . array_shift($bang);
-}
-
-// Get coordinates
-$start_point = array_shift($bang);
-$end_point = array_pop($bang);
-
-echo "START: " . $grid[234][771] . "\n";
-
-if ($command === "turn on") {
-    lights_on($start_point, $end_point);
-}
-
-echo "END: " . $grid[234][771];
-
-function lights_on($start, $end) {
-    global $grid;
-
-    for ($i = $start[0]; $i <= $end[0]; $i++) {
-        for ($j = $start[1]; $j <= $end[1]; $j++) {
-            $grid[$i][$j] = 1;
-        }
+foreach($instructions as $line) {
+    // Begin parsing string
+    $bang = explode(' ', $line);
+    
+    // Grab command from input
+    $command = array_shift($bang);
+    if ($command === "turn") {
+        $command .= " " . array_shift($bang);
     }
+    
+    // Get coordinates
+    $start_point = explode(',', array_shift($bang));
+    $end_point = explode(',', array_pop($bang));
+    
+    change_lights($command, $start_point, $end_point);
 }
+    
+echo count_lights();
 
 function change_lights($cmd, $start, $end) {
     global $grid;
@@ -48,4 +43,19 @@ function change_lights($cmd, $start, $end) {
             }
         }
     }
+}
+
+function count_lights() {
+    global $grid;
+
+    $count = 0;
+    for ($i = 0; $i <= 999; $i++) {
+        for ($j = 0; $j <= 999; $j++) {
+            if ($grid[$i][$j]) {
+                $count++;
+            }
+        }
+    }
+
+    return $count;
 }
